225836e445479e059407f73332527f21bb109f03
Karen Etheridge <ketheridge@campusexplorer.com>
2013-07-24 23:06:08 +0000

    fix locale by swapping out generic zzz.sh for a custom one

--- install/ouzo/configs/.profile.d/zzz.sh
+++ install/ouzo/configs/.profile.d/zzz.sh
@@ -1,1 +1,8 @@
-../../../generic/configs/.profile.d/zzz.sh
+# stuff that runs at the (presumed) end of the .sh startup
+
+# do generic version first
+source ~/git/misc/install/generic/configs/.profile.d/zzz.sh
+
+# fix locale - en_US isn't available here? o.O
+export LANG=C
+

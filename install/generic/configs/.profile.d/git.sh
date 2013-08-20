308e675b9b89bc76057da3fb5369c384728acd47
Karen Etheridge <ether@cpan.org>
2013-08-20 16:53:26 -0700

    make "git new-workdir" available

--- install/generic/configs/.profile.d/git.sh
+++ install/generic/configs/.profile.d/git.sh
@@ -7,3 +7,6 @@ alias gpr='git pull --rebase'
 if [ -f ~/.git-completion.bash ]; then
   . ~/.git-completion.bash
 fi
+
+# this is only valid for OSX, but harmless to include everywhere...
+export PATH=$PATH:/opt/local/share/git-core/contrib/workdir

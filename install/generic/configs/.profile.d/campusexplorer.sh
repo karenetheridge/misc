2a1315bfd394a08c5930e359fa990f902cf3100d
Karen Etheridge <ether@cpan.org>
2014-04-27 09:55:31 -0700

    alias for grepping all interesting bits of the codebase

--- install/generic/configs/.profile.d/campusexplorer.sh
+++ install/generic/configs/.profile.d/campusexplorer.sh
@@ -5,3 +5,8 @@ export CEROOT=~/src/CE
 export CE_EMAIL_ADDRESS=ketheridge@campusexplorer.com
 
 export PATH=/Users/ether/src/CE/devtools:/Users/ether/git/CE/devtools:/Users/ether/src/CE/bin:/Users/ether/git/CE/bin:$PATH
+
+ackall() {
+    ack "$@" \
+        lib t tlib bin root root-admin extlib/etc/modules.yaml
+}

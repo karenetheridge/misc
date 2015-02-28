71ebbf4434d8741e8e64ca373fd90afcc0636d6c
Karen Etheridge <ether@cpan.org>
2015-02-27 18:32:19 -0800

    do not expire from the keychain so soon

--- install/generic/configs/.profile.d/ssh.sh
+++ install/generic/configs/.profile.d/ssh.sh
@@ -2,7 +2,7 @@
 fix_ssh_agent()
 {
     eval $(ssh-agent)
-    ssh-add -K ~/.ssh/id_rsa
+    ssh-add -t 25920000 -K /Users/ether/.ssh/id_rsa
 }
 
 # having troubles getting the passphrase remembered on a headless OSX session?

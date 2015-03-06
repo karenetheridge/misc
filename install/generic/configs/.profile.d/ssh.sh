92679bc7e4a5edbd6c9b9cd324d34b642cf6e053
Karen Etheridge <ether@cpan.org>
2015-03-06 11:21:42 -0800

    reuse existing ssh agents when I log in remotely

--- install/generic/configs/.profile.d/ssh.sh
+++ install/generic/configs/.profile.d/ssh.sh
@@ -2,8 +2,27 @@
 fix_ssh_agent()
 {
     eval $(ssh-agent)
-    ssh-add -t 25920000 -K ~ether/.ssh/id_rsa
+    ssh-add -t 25920000 -K ~/.ssh/id_rsa
 }
 
 # having troubles getting the passphrase remembered on a headless OSX session?
 # try this:  ssh-add -K ~/.ssh/id_rsa
+
+
+
+# adapted from
+# https://superuser.com/questions/141044/sharing-the-same-ssh-agent-among-multiple-login-sessions
+
+# attempt to connect to a running agent - cache SSH_AUTH_SOCK in ~/.ssh/
+sagent()
+{
+    [ -S "$SSH_AUTH_SOCK" ] || export SSH_AUTH_SOCK="$(< ~/.ssh/ssh-agent.env)"
+
+    # if cached agent socket is invalid, start a new one
+    [ -S "$SSH_AUTH_SOCK" ] || {
+        eval "$(ssh-agent)"
+        ssh-add -t 25920000 -K ~/.ssh/id_rsa
+        echo "$SSH_AUTH_SOCK" > ~/.ssh/ssh-agent.env
+    }
+}
+

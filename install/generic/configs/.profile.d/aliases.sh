0e96af4a0b6e571f6cdb55d3ae2f85f38d0c37f5
Karen Etheridge <ether@cpan.org>
2015-03-29 11:38:34 -0700

    consolidate aliases and simple functions into one file

--- install/generic/configs/.profile.d/aliases.sh
+++ install/generic/configs/.profile.d/aliases.sh
@@ -42,3 +42,26 @@ alias sidediff='colordiff  -s --side-by-side --width=165'
 alias units=gunits
 
 alias temp='/Applications/TemperatureMonitor.app//Contents/MacOS/tempmonitor -a -l | grep "BATTERY POSITION 2"'
+
+
+
+
+mtime() {
+    perl -wle'open my $fh, "<", $ARGV[0]; print +(stat($fh))[9];'
+}
+
+atime() {
+    perl -wle'open my $fh, "<", $ARGV[0]; print +(stat($fh))[8];'
+}
+
+
+title() {
+    printf "\033]0;%s\007" "$1"
+}
+
+# find non-ascii content
+alias ackutf8='ack "[^[:ascii:]]"'
+alias nonascii='ack "[^[:ascii:]]"'
+
+alias crontab="VIM_CRONTAB=true crontab"
+

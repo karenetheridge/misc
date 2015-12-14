b43db40cd51574d7712a4f0b326e270719e389e2
Karen Etheridge <ether@cpan.org>
2015-12-13 18:40:44 -0800

    perl 5 version 22.1 is now on deck

--- install/generic/configs/.profile.d/perl.sh
+++ install/generic/configs/.profile.d/perl.sh
@@ -328,7 +328,7 @@ poo() {
 
 # run the command on all major perlbrews, starting at 8.8
 almostallperls() {
-    for perl in 8.8 8.9 10.0 10.1 12.5 14.4 16.3 18.4 20.3 22.0 23.5 ; do
+    for perl in 8.8 8.9 10.0 10.1 12.5 14.4 16.3 18.4 20.3 22.1 23.5 ; do
         perlbrew use ${perl}@std;
         echo; echo using $PERL5LIB
         eval $(printf "%q " "$@")
@@ -338,7 +338,7 @@ almostallperls() {
 }
 
 allperls () {
-    for perl in 8.1 8.5 8.8 8.9 10.0 10.1 12.5 14.4 16.3 18.4 20.3 22.0 23.5 ; do
+    for perl in 8.1 8.5 8.8 8.9 10.0 10.1 12.5 14.4 16.3 18.4 20.3 22.1 23.5 ; do
         perlbrew use ${perl}@std;
         echo; echo using $PERL5LIB
         eval $(printf "%q " "$@")

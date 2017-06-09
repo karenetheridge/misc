9f3ec228e2ee85c41353bace71122c82344f0942
Karen Etheridge <ketheridge@campusexplorer.com>
2017-06-09 15:32:18 -0700

    save uncommitted changes in vagrant installation

--- install/vagrant-sandbox/configs/.profile.d/campusexplorer.sh
+++ install/vagrant-sandbox/configs/.profile.d/campusexplorer.sh
@@ -4,7 +4,15 @@ export CEROOT=/vagrant
 export CE_EMAIL_ADDRESS=ketheridge@campusexplorer.com
 export CE_SSH_TIMEOUT=120
 
+export CE_MONDAYS_STYLE=safe
+
+# make Test::Class print the test names
+export TEST_VERBOSE=1
+
 eval $(perl -Mlocal::lib=/vagrant/extlib)
+
+# do this to bring in my other local-lib with extra stuff.
+#eval $(perl -Mlocal::lib=/vagrant/etherlib)
 
 export CHEOPSROOT=/home/vagrant/git/cheops
 
@@ -26,5 +34,25 @@ db_credentials() {
 
 dsn() {
     perl -Ilib -MCE::Config -wle'print CE::Config->dsn($ARGV[0] // q{localdev}, $ARGV[1] // q{cedev})' $1 $2
+}
+
+ceflow() {
+    tput bel
+    tput bel
+    echo "tried to run: ceflow $*"
+    tput bel
+    tput bel
+}
+
+fix_grants() {
+    echo 'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES, LOCK TABLES, SHOW VIEW ON `cedev`.* TO "ce_web"@"%"' | devtools/db
+}
+
+aws_upload() {
+    filename="$1"
+    zip $filename.zip $filename
+    aws s3 cp $filename.zip s3://tmp.campusexplorer.com
+    bin/s3-authenticated-url --expires 1440 tmp.campusexplorer.com/`basename $filename.zip`
+    tput bel
 }
 

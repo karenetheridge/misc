63c14a54e9de6681ec5556379b3b18b715070e5a
Karen Etheridge <ether@joyent.com>
2018-07-09 14:51:34 -0700

    remove another unwanted host

--- install/vagrant-sandbox/configs/.profile.d/perl.sh
+++
@@ -1,4 +0,0 @@
-source ~/git/misc/install/generic/configs/.profile.d/perl.sh
-
-# only use Gist for work stuff
-export NOPASTE_SERVICES="Gist"
--- install/vagrant-sandbox/configs/.profile.d/zzz.sh
+++
@@ -1,8 +0,0 @@
-# stuff that runs at the (presumed) end of the .sh startup
-
-# do generic version first
-source ~/git/misc/install/generic/configs/.profile.d/zzz.sh
-
-# fix locale - en_US isn't available here? o.O
-export LANG=C
-

434ce815a9c6b3eb087e5033305d0a90e044a18a
Karen Etheridge <ether@cpan.org>
2018-07-10 14:15:54 -0700

    more configs, from buildops-docs

--- install/aquavit/configs/.profile.d/conch.sh
+++ install/aquavit/configs/.profile.d/conch.sh
@@ -3,3 +3,6 @@ export POSTGRES_HOME=/opt/local/var/db/postgresql96/defaultdb
 # this should be automatic, but see https://trac.macports.org/ticket/50058
 export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
 
+# from buildops-docs/setup/mac/
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$PATH

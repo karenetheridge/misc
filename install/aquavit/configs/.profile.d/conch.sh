b6946ca681fec87cc9321a652bded6dec5d38728
Karen Etheridge <ether@cpan.org>
2018-07-10 13:19:41 -0700

    new file for Conch-specific dependencies

---
+++ install/aquavit/configs/.profile.d/conch.sh
@@ -0,0 +1,5 @@
+export POSTGRES_HOME=/opt/local/var/db/postgresql96/defaultdb
+
+# this should be automatic, but see https://trac.macports.org/ticket/50058
+export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
+

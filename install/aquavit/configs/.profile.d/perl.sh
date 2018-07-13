7df3e5a310a9dca7cf9c842bc249a3dd92e6954f
Karen Etheridge <ether@cpan.org>
2018-07-13 08:12:50 -0700

    disable testing in parallel here

--- install/aquavit/configs/.profile.d/perl.sh
+++ install/aquavit/configs/.profile.d/perl.sh
@@ -3,3 +3,6 @@ source /Users/ether/git/misc/install/generic/configs/.profile.d/perl.sh
 export CPAN_META_JSON_BACKEND=
 
 unset PERL_USE_UNSAFE_INC
+
+# some things, e.g. DBD::Pg, don't parallelize well in tests
+export HARNESS_OPTIONS=j1:c

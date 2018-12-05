e8a93b081b32f99e00ad396756532018d98067c0
Karen Etheridge <ether@cpan.org>
2018-12-05 11:46:56 -0800

    tests are parallelizable now (at least in my main devline)

--- install/aquavit/configs/.profile.d/perl.sh
+++ install/aquavit/configs/.profile.d/perl.sh
@@ -5,6 +5,3 @@ source /Users/ether/git/misc/install/generic/configs/.profile.d/perl.sh
 export CPAN_META_JSON_BACKEND=
 
 unset PERL_USE_UNSAFE_INC
-
-# some things, e.g. DBD::Pg, don't parallelize well in tests
-export HARNESS_OPTIONS=j1:c

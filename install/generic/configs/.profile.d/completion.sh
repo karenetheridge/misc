b4fe835b341c15b8cd7b42e2c1f9050ca60c1cfc
Karen Etheridge <ether@cpan.org>
2019-07-17 12:33:06 -0700

    enable extended globbing

--- install/generic/configs/.profile.d/completion.sh
+++ install/generic/configs/.profile.d/completion.sh
@@ -5,3 +5,6 @@
 # completion for Makefile targets
 # see http://stackoverflow.com/a/38415982/1472048
 complete -W "\`grep -oE '^[a-zA-Z0-9_-]+:([^=]|$)' Makefile | sed 's/[^a-zA-Z0-9_-]*$//'\`" make
+
+# enable extended globbing! (see 'extglob' in man bash)
+shopt -s extglob

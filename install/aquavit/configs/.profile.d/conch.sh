aa3f14495398ada3e357a527ea8bd405bec170ee
Karen Etheridge <ether@cpan.org>
2019-11-28 10:46:03 -0800

    upgrade to postgres 10

--- install/aquavit/configs/.profile.d/conch.sh
+++ install/aquavit/configs/.profile.d/conch.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bi
 
 # see https://stackoverflow.com/questions/11618898/pg-config-executable-not-found
 # allows pg_config to be found and run, to set POSTGRES_* environment variables
-export PATH=$PATH:/opt/local/lib/postgresql96/bin
+export PATH=$PATH:/opt/local/lib/postgresql10/bin
 
 # from buildops-docs/setup/mac/
 export GOPATH=$HOME/go

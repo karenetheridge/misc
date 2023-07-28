6fa408c6c4a8073a1f8352b7bb10691b1488bdde
Karen Etheridge <ether@cpan.org>
2023-07-28 10:39:20 -0700

    new alias for "all commits since master"

--- install/generic/.profile.d/git.sh
+++ install/generic/.profile.d/git.sh
@@ -8,6 +8,7 @@ alias gd='git diff'
 alias gdd='git diff $(git describe --abbrev=0 --tags) HEAD' # committed changes since last tag
 alias gds='git diff --staged'
 alias gdo='git diff @{u}'
+alias glpo='glp --reverse origin/master..HEAD'
 alias gs='git status'
 alias gsi='git status --ignored'
 alias glp='git log -p --decorate --notes --pretty=fuller --stat'

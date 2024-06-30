5c1cb829301851c102a5e25f7d06309aceca3a46
Karen Etheridge <ether@cpan.org>
2024-06-29 17:45:20 -0700

    use quotes correctly

--- install/generic/.profile.d/git-hub.sh
+++ install/generic/.profile.d/git-hub.sh
@@ -2,5 +2,5 @@
 
 # see https://github.com/ingydotnet/git-hub
 
-export PATH=$HOME/git/git-hub/lib:"$PATH"
-export MANPATH=$HOME/git/git-hub/man:"$MANPATH"
+export PATH="$HOME/git/git-hub/lib:$PATH"
+export MANPATH="$HOME/git/git-hub/man:$MANPATH"
